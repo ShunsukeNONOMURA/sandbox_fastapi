f80559bb50d8c5e3c71e6b0da48ffea23fe706fc
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2025-05-28 16:40:54 +0900

    add mcp

--- volume/app/main.py
+++ volume/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
 
 from app.endpoint.router import main_router
 
 app = FastAPI()
 app.include_router(main_router)
+
+mcp = FastApiMCP(
+    app,
+    name="My API MCP",
+    description="My API description",
+    # base_url="http://localhost:8000",
+)
+
+# Mount the MCP server directly to your FastAPI app
+mcp.mount()
