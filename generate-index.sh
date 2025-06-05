fd40b9fd882a80cddf22f2615c02cc3645a0d31e
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2025-06-05 10:03:54 +0900

    fix generate index shell

--- generate-index.sh
+++ generate-index.sh
@@ -2,13 +2,14 @@
 
 OUTPUT=index.html
 VERSIONS_DIR=versions
+BASE_URL=$(basename `git config --get remote.origin.url` .git)
 
 echo '<!DOCTYPE html>' > $OUTPUT
 echo '<html lang="ja"><head><meta charset="UTF-8"><title>ドキュメントバージョン一覧</title></head><body>' >> $OUTPUT
 echo '<h1>ドキュメントバージョン一覧</h1><ul>' >> $OUTPUT
 
 for version in $(ls -1 $VERSIONS_DIR); do
-  BASE="/versions/$version"
+  BASE="/$BASE_URL/versions/$version"
 
   echo "<li><strong>$version</strong><ul>" >> $OUTPUT
 
