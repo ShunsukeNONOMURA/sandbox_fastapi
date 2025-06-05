d564865deab81d786f70f2b49adda577514ceca3
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2025-06-05 09:42:12 +0900

    add index.html

---
+++ generate-index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OUTPUT=tmp_deploy/index.html
+VERSIONS_DIR=tmp_deploy/versions
+
+echo '<!DOCTYPE html>' > $OUTPUT
+echo '<html lang="ja"><head><meta charset="UTF-8"><title>ドキュメントバージョン一覧</title></head><body>' >> $OUTPUT
+echo '<h1>ドキュメントバージョン一覧</h1><ul>' >> $OUTPUT
+
+for version in $(ls -1 $VERSIONS_DIR); do
+  BASE="/versions/$version"
+
+  echo "<li><strong>$version</strong><ul>" >> $OUTPUT
+
+  [[ -d "$VERSIONS_DIR/$version/site" ]] && echo "<li><a href=\"$BASE/site/\">📘 MkDocs</a></li>" >> $OUTPUT
+  [[ -d "$VERSIONS_DIR/$version/redoc" ]] && echo "<li><a href=\"$BASE/redoc/\">📕 ReDoc</a></li>" >> $OUTPUT
+  [[ -d "$VERSIONS_DIR/$version/htmlcov" ]] && echo "<li><a href=\"$BASE/htmlcov/\">📊 Coverage</a></li>" >> $OUTPUT
+
+  echo "</ul></li>" >> $OUTPUT
+done
+
+echo '</ul></body></html>' >> $OUTPUT
