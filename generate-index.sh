3c71038ff98ff9c57a597a60b02b2488ee327428
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2025-06-05 10:10:06 +0900

    fix generate index shell

--- generate-index.sh
+++ generate-index.sh
@@ -8,16 +8,16 @@ echo '<!DOCTYPE html>' > $OUTPUT
 echo '<html lang="ja"><head><meta charset="UTF-8"><title>ドキュメントバージョン一覧</title></head><body>' >> $OUTPUT
 echo '<h1>ドキュメントバージョン一覧</h1><ul>' >> $OUTPUT
 
-for version in $(ls -1 $VERSIONS_DIR); do
-  BASE="/$BASE_URL/versions/$version"
+for version_dir in $(find $VERSIONS_DIR -mindepth 1 -maxdepth 10 -type d); do
+  version_path=${version_dir#$VERSIONS_DIR/}  # versions/ を除去
+  BASE="/$BASE_URL/$VERSIONS_DIR/$version_path"
 
-  echo "<li><strong>$version</strong><ul>" >> $OUTPUT
+  echo "<li><strong>$version_path</strong><ul>" >> $OUTPUT
 
-  [[ -d "$VERSIONS_DIR/$version/site" ]] && echo "<li><a href=\"$BASE/site/\">📘 MkDocs</a></li>" >> $OUTPUT
-  [[ -d "$VERSIONS_DIR/$version/redoc" ]] && echo "<li><a href=\"$BASE/redoc/\">📕 ReDoc</a></li>" >> $OUTPUT
-  [[ -d "$VERSIONS_DIR/$version/htmlcov" ]] && echo "<li><a href=\"$BASE/htmlcov/\">📊 Coverage</a></li>" >> $OUTPUT
+  [[ -d "$version_dir/site" ]] && echo "<li><a href=\"$BASE/site/\">📘 MkDocs</a></li>" >> $OUTPUT
+  [[ -d "$version_dir/redoc" ]] && echo "<li><a href=\"$BASE/redoc/\">📕 ReDoc</a></li>" >> $OUTPUT
+  [[ -d "$version_dir/htmlcov" ]] && echo "<li><a href=\"$BASE/htmlcov/\">📊 Coverage</a></li>" >> $OUTPUT
 
   echo "</ul></li>" >> $OUTPUT
 done
-
 echo '</ul></body></html>' >> $OUTPUT
